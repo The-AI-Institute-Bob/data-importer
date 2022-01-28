05aaefc21a10fc6a4bf663a73378aa0a8475fd71
Nicolas RENNERT <nrennert@objecthub.fr>
2022-01-28 12:40:13 +0100

    add ascor

---
+++ Node/messagesToQuestions.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+node msgToQA.js --db=bob --user=postgres --host=db-core.czcdgzwouwz1.eu-west-3.rds.amazonaws.com --password 'w36#wdcE12?' --corpus_id=140 --course_id=19 --lang=fr  --quality=unknown --relevancy=1  --visibility --chapter_id=70 --tag_id=8
+node msgToQA.js --db=bob --user=postgres --host=db-core.czcdgzwouwz1.eu-west-3.rds.amazonaws.com --password 'w36#wdcE12?' --corpus_id=141 --course_id=18 --lang=fr  --quality=unknown --relevancy=1  --visibility --chapter_id=69 --tag_id=8
+node msgToQA.js --db=bob --user=postgres --host=db-core.czcdgzwouwz1.eu-west-3.rds.amazonaws.com --password 'w36#wdcE12?' --corpus_id=142 --course_id=21 --lang=fr  --quality=unknown --relevancy=1  --visibility --chapter_id=72 --tag_id=8
+
+node msgToQA.js --db=bob --user=postgres --host=db-core.czcdgzwouwz1.eu-west-3.rds.amazonaws.com --password 'w36#wdcE12?' --corpus_id=146 --course_id=12 --lang=en  --quality=unknown --relevancy=1  --visibility --chapter_id=61 --tag_id=8
+
+node msgToQA.js --db=bob --user=postgres --host=db-core.czcdgzwouwz1.eu-west-3.rds.amazonaws.com --password 'w36#wdcE12?' --corpus_id=147 --course_id=13 --lang=en  --quality=unknown --relevancy=1  --visibility --chapter_id=63 --tag_id=8
+
+node msgToQA.js --db=bob --user=postgres --host=db-core.czcdgzwouwz1.eu-west-3.rds.amazonaws.com --password 'w36#wdcE12?' --corpus_id=152 --course_id=8 --lang=fr  --quality=unknown --relevancy=1  --visibility --chapter_id=74 --tag_id=8
+
+node msgToQA.js --db=bob --user=postgres --host=db-core.czcdgzwouwz1.eu-west-3.rds.amazonaws.com --password 'w36#wdcE12?' --corpus_id=153 --course_id=28 --lang=fr  --quality=unknown --relevancy=1  --visibility --chapter_id=80 --tag_id=8
